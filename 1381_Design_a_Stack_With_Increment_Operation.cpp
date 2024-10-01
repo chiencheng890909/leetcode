42a21e4275e6e28eb5103e0c7758da63f6ea581d
Chien-Cheng, Su <56793992+chiencheng890909@users.noreply.github.com>
2024-10-01 17:52:30 +0800

    Create 1381_Design_a_Stack_With_Increment_Operation.cpp
    Runtime Beats 88.68%, Memory Beats 8.75%

---
+++ 1381_Design_a_Stack_With_Increment_Operation.cpp
@@ -0,0 +1,67 @@
+struct Node {
+    int val = 0;
+    Node *next = nullptr;
+    Node *prev = nullptr;
+};
+
+
+class CustomStack {
+private:
+    Node * Head;
+    Node * Tail;
+    int Size;
+    int Max_Size;
+public:
+    CustomStack(int maxSize) {
+        Head = new Node();
+        Tail = Head;
+        Max_Size = maxSize;
+        Size = 0;
+    }
+    
+    void push(int x) {
+        if(Size < Max_Size) {
+            Node* add = new Node();
+
+            add->prev = Tail;
+            add->val = x;
+            Tail->next = add;
+            Tail = add;
+            Size++;
+        }
+    }
+    
+    int pop() {
+        if(Size > 0) {
+            Node* Del = Tail;
+            int temp = Del->val;
+            Tail = Tail->prev;
+            Tail->next = nullptr;
+            delete Del;
+            Size--;
+            return temp;
+        }
+        else
+            return -1;
+    }
+    
+    void increment(int k, int val) {
+        Node* it = Head->next;
+        for(int i = 0; i < k; i++) {
+            if(it == nullptr)
+                break;
+            else {
+                it->val = it->val + val;
+                it = it->next;
+            }
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
