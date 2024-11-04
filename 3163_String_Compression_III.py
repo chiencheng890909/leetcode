208e3b70b85b7c4124cd28f3e439f83159e509f8
Chien-Cheng, Su <56793992+chiencheng890909@users.noreply.github.com>
2024-11-04 14:19:25 +0800

    Runtime Beats 95.04%, Memory Beats 85.91%

--- 3163_String_Compression_III.py
+++ 3163_String_Compression_III.py
@@ -1,9 +1,9 @@
 class Solution:
     def compressedString(self, word: str) -> str:
         num = 1
-        char = list(word)[0]
+        char = word[0]
         Ans = ""
-        for c in list(word)[1:]:
+        for c in word[1:]:
             if c == char and num < 9:
                 num += 1
             else:
